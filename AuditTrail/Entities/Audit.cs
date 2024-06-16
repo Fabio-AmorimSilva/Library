72db9f076dfb9909fe00ca6112396770b9ec51d9
Fabio <fabioamorin120@gmail.com>
2024-06-16 15:54:04 -0300

    feat: Adding new interceptor

--- AuditTrail/Entities/Audit.cs
+++ AuditTrail/Entities/Audit.cs
@@ -11,4 +11,26 @@ public class Audit
     public string NewValues { get; private set; }
     public string UpdatedColumns { get; private set; }
     public string PrimaryKey { get; private set; }
+
+    public Audit(
+        Guid userId, 
+        string type, 
+        string tableName, 
+        DateTime timeStamp, 
+        string oldValues, 
+        string newValues, 
+        string updatedColumns, 
+        string primaryKey
+    )
+    {
+        Id = Guid.NewGuid();
+        UserId = userId;
+        Type = type;
+        TableName = tableName;
+        TimeStamp = timeStamp;
+        OldValues = oldValues;
+        NewValues = newValues;
+        UpdatedColumns = updatedColumns;
+        PrimaryKey = primaryKey;
+    }
 }
---
+++ AuditTrail/Interceptors/AuditEntityInterceptor.cs
@@ -0,0 +1,49 @@
+﻿using AuditTrail.Entities;
+using Library.Core.Entities;
+using Library.Core.Entities.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuditTrail.Interceptors;
+
+public class AuditEntityInterceptor : SaveChangesInterceptor
+{
+    public override async ValueTask<int> SavedChangesAsync(
+        SaveChangesCompletedEventData eventData, 
+        int result,
+        CancellationToken cancellationToken = new CancellationToken()
+    )
+    {
+        await SetAuditableEntity(eventData.Context);
+        
+        return await base.SavedChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private async Task SetAuditableEntity(DbContext? context)
+    {
+        if (context is null)
+            return;
+        
+        var entries = context.ChangeTracker
+            .Entries<BaseEntity>()
+            .ToList();
+
+        if (entries.Count != 0)
+        {
+            foreach (var entry in entries)
+            {
+                var audit = new Audit(
+                    userId: entry.Entity.CreatedBy,
+                    type: entry.Entity.GetType().ToString(),
+                    tableName: entry.Entity.GetType() + "s",
+                    timeStamp: DateTime.Now, 
+                    oldValues: entry.OriginalValues.Properties.ToString(),
+                    newValues: entry.CurrentValues.Properties.ToString(),
+                    updatedColumns: entry.CurrentValues.Properties.Select(p => p.Name).ToString(),
+                    primaryKey: entry.Entity.Id.ToString()
+                );
+                
+                await context.AddAsync(audit);
+            }
+        }
+    }
+}
