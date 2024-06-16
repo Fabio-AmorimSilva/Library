d1b599c496c02221ec0d461f87b2e0ebc12ce4e5
Fabio <fabioamorin120@gmail.com>
2024-06-16 12:59:49 -0300

    refactor: Updating to primary constructor

--- Library.Infrastructure/LibraryContext.cs
+++ Library.Infrastructure/LibraryContext.cs
@@ -1,15 +1,11 @@
 ﻿namespace Library.Infrastructure;
 
-public class LibraryContext : BaseContext
+public class LibraryContext(DbContextOptions<BaseContext> options) : BaseContext(options)
 {
     public DbSet<Author> Authors {  get; set; }
     public DbSet<Book> Books {  get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<LibraryUnit> Libraries { get; set; }
-    
-    public LibraryContext(DbContextOptions<BaseContext> options) : base(options)
-    {
-    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
         => modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
