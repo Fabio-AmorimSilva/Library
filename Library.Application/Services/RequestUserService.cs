e95400b425f84f0b94fee0217b895a24413a99ee
Fabio <fabioamorin120@gmail.com>
2024-06-30 11:55:10 -0300

    feat: Creating new service

---
+++ Library.Application/Services/RequestUserService.cs
@@ -0,0 +1,11 @@
+﻿namespace Library.Application.Services;
+
+public class RequestUserService(IHttpContextAccessor accessor)
+{
+    public string GetUserId()
+    {
+        var userId = accessor.HttpContext?.User.Claims.FirstOrDefault(u => u.Type == ClaimTypes.Sid)?.Value;
+
+        return userId ?? string.Empty;
+    }
+}
